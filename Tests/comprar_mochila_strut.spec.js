05e270bdfbc19404b8bd93cdc1c5b2bd74a97f39
cleysonalves28 <calves_nascimento@hotmail.com>
2025-10-20 21:56:14 -0300

    Final do projeto - script Professor

--- Tests/comprar_mochila_strut.spec.js
+++ Tests/comprar_mochila_strut.spec.js
@@ -1,121 +1,124 @@
 import { test, expect } from '../utils/logger.js'
 import { snap } from '../utils/snap.js'
-
+ 
 // Funções de apoio
-async function login_step(page, testInfo){
+async function login_step(page){
     await page.goto('/')
-
+ 
     await expect(page).toHaveURL('/') // Verificação clássica
     await expect(page.locator('[data-test="login-button"]')).toHaveText('Login')
-    
-    await snap(page, testInfo, 'TC001-Passo01-Home')
+   
 }
-
-async function success_login_step(page, testInfo) {
+ 
+async function success_login_fill_step(page) {
     await page.locator('[data-test="username"]').fill('standard_user')
     await page.locator('[data-test="password"]').fill('secret_sauce')
-    await snap(page, testInfo, 'TC001-Passo02A-Login_Preenchido')
+}
+ 
+async function success_login_click_step(page){
     await page.locator('[data-test="login-button"]').click()
-
+ 
     await expect(page).toHaveURL(/inventory\.html/)
     await expect(page.locator('[data-test="title"]')).toHaveText('Products')
-    
-    await snap(page, testInfo, 'TC001-Passo02B-Inventory')
+       
 }
-
-
+ 
+async function add_to_cart_step(page){
+    await page.locator('[data-test="shopping-cart-link"]').click()
+    await expect(page).toHaveURL(/cart\.html/)
+    await expect(page.locator('[data-test="title"]')).toHaveText("Your Cart")
+    // await expect(page.locator('[data-test="title"]')).toContainText("Cart")
+    await expect(page.locator('[data-test="item-quantity"]')).toHaveText("1")
+    await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText("Sauce Labs Backpack")
+    await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText("$29.99")
+}
+ 
 test.describe('SauceDemo - fluxo principal de compra', () => {
-    test('Comprar Mochila Direto', 
+    test('Comprar Mochila Direto',
         async({ page }, testInfo) => {
         testInfo.setTimeout(testInfo.timeout + 15000)
-        
+       
         // Inicio do Passo 1
         await test.step('Acessar SauceDemo.com', async () => {
-            await login_step(page, testInfo)            
+            await login_step(page)
+            await snap(page, testInfo, 'TC001-Passo01-Home')            
         }) // fim do passo 1
-
+ 
         // Inicio do passo 2
         await test.step('Login com Sucesso', async () => {
-            success_login_step(page, testInfo)
+            await success_login_fill_step(page) // preenche email e senha
+            await snap(page, testInfo, 'TC001-Passo02A-Login_Preenchido')
+            await success_login_click_step(page) // clique para entrar
+            await snap(page, testInfo, 'TC001-Passo02B-Inventory')
         }) // fim do passo 2
-
+ 
         // Inicio do passo 3
         await test.step('Adicionar mochila no carrinho', async () => {
             const seletor_mochila = page.locator('.inventory_item').filter({ hasText: /Backpack/ })
             await seletor_mochila.getByRole('button', { name: /Add to cart/ }).click()
-
+ 
             await expect(page.locator('.shopping_cart_badge')).toHaveText('1')
             await snap(page, testInfo, 'TC001-Passo03-Mochila-Adicionada')
         }) // fim do passo 3
-
+ 
         await test.step('Ir para o carrinho', async () => {
-            await page.locator('[data-test="shopping-cart-link"]').click()
-            await expect(page).toHaveURL(/cart\.html/)
-            await expect(page.locator('[data-test="title"]')).toHaveText("Your Cart")
-            // await expect(page.locator('[data-test="title"]')).toContainText("Cart")
-            await expect(page.locator('[data-test="item-quantity"]')).toHaveText("1")
-            await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText("Sauce Labs Backpack")
-            await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText("$29.99")
+            await add_to_cart_step(page)
             await snap(page, testInfo, 'TC001-Passo4-Carrinho-Conferido')
         })
-
-        await test.step('Clicar no botão Checkout', async() => {
-            await page.locator('[data-test="checkout"]').click()
-            await expect(page).toHaveURL(/.checkout-step-one\.html/)
-            await expect(page.locator('.title')).toHaveText('Checkout: Your Information')
-            await page.locator('#first-name').fill('Cleyson Alves')
-            await page.locator('#last-name').fill('Alves')
-            await page.locator('#postal-code').fill('0670-78')
-            await page.locator('#continue').click()
-            await snap(page, testInfo, 'TC001-Passo05-Checkout_preenchido')
-
-        })
-
-        await test.step('Clicar botão Finish', async() => {
-            await expect(page).toHaveURL(/.checkout-step-two\.html/)
-            await expect(page.locator('.title')).toHaveText('Checkout: Overview')
-            await expect(page.locator('[data-test="item-quantity"]')). toHaveText('1')
-            await expect(page.locator('[data-test="inventory-item-price"]')). toHaveText('$29.99')
-            await expect(page.locator('[data-test="total-label"]')).toHaveText('Total: $32.39')
-            await snap(page, testInfo, 'TC001-Passo06-Checkout_Overview')
-            await page.locator('#finish'). click()
-                        
-        })
-
-        await test.step('Mensagem de agradecimento', async() => {
-            await expect(page).toHaveURL(/.checkout-complete\.html/)
-            await expect(page.locator('.title')).toHaveText('Checkout: Complete!')
-            await expect(page.locator('[data-test="complete-header"]')).toHaveText('Thank you for your order!')
-            await snap(page, testInfo, 'TC001-Passo06-Checkout_Overview')
-
-            await page.waitForTimeout(4000)
-
-        })
-
+ 
     }) // fim do test 1
-    // test('Comprar Mochila Detalhes', 
-    //     async({ page }, testInfo) => {
-    //     testInfo.setTimeout(testInfo.timeout + 15000)
-        
-    //     // Inicio do Passo 1
-    //     await test.step('Acessar SauceDemo.com', async () => {
-    //         await login_step(page, testInfo)            
-    //     }) // fim do passo 1
+ 
+    // inicio do test 2
+    test('Comprar Mochila Detalhes',
+        async({ page }, testInfo) => {
+        testInfo.setTimeout(testInfo.timeout + 15000)
+       
+        // Inicio do Passo 1
+        await test.step('Acessar SauceDemo.com', async () => {
+            await login_step(page)  
+            await snap(page, testInfo, 'TC002-Passo01-Home')          
+        }) // fim do passo 1
+ 
+        // Inicio do passo 2
+        await test.step('Login com Sucesso', async () => {
+            await success_login_fill_step(page) // preenche email e senha
+            await snap(page, testInfo, 'TC002-Passo02A-Login_Preenchido')
+            await success_login_click_step(page) // clique para entrar
+            await snap(page, testInfo, 'TC002-Passo02B-Inventory')
+       
+        }) // fim do passo 2
+ 
+        // Inicio do passo 3
+        await test.step('Abrir pagina da mochila e adicionar no carrinho', async () => {
+            // Parte 3.1 - Abrir a pagina da mochila
+            // ação // código de produto: 4 = mochila
+            await page.locator('[data-test="item-4-title-link"]').click()                       
+            // verificações
+            // Estamos na pagina certa?
+            await expect(page).toHaveURL(/inventory-item\.html/)   // url
+            await expect(page).toHaveTitle('Swag Labs')             // title (guia)
+            await expect(page.locator('[data-test="back-to-products"]')).toHaveText('Back to products') // text
+            // As informações do produto estão certas?
+            await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText("Sauce Labs Backpack")
+            await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText("$29.99")
+           
+            await snap(page, testInfo, 'TC002-Passo03_1-Inventory_Item')
+ 
+            // Parte 3.2 - Adicionar produto no carrinho
+            // ação
+            await page.locator('[data-test="add-to-cart"]').click()
+            // verificações
+            await expect(page.locator('.shopping_cart_badge')).toHaveText('1')
+            await snap(page, testInfo, 'TC002-Passo03_2-Mochila-Adicionada')
+        }) // fim do passo 3
+ 
+        await test.step('Ir para o carrinho', async () => {
+            add_to_cart_step(page)
+            await snap(page, testInfo, 'TC002-Passo4-Carrinho-Conferido')
 
-    //     // Inicio do passo 2
-    //     await test.step('Login com Sucesso', async () => {
-    //         success_login_step(page, testInfo)
-    //     }) // fim do passo 2
-
-    //     // Inicio do passo 3
-    //     await test.step('Adicionar mochila no carrinho', async () => {
-    //         const seletor_mochila = page.locator('.inventory_item').filter({ hasText: /Backpack/ })
-    //         await seletor_mochila.getByRole('link', { hasText: /Backpack/ }).click()
-    //         // To Do: Concluir
-    //         await expect(page.locator('.shopping_cart_badge')).toHaveText('1')
-    //         await snap(page, testInfo, 'TC001-Passo03-Mochila-Adicionada')
-    //     }) // fim do passo 3
-        
-
-    // }) // fim do test 2
+        })
+       
+       
+ 
+    }) // fim do test 2
 }) // fim do describe
